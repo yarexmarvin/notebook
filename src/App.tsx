7018cdaab42687c7e52aee798bef7eaa21f7e8b0
yarex <yarexmason@gmail.com>
2021-10-25 12:16:35 +0400

    save notes on localStorage using redux-saga

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,8 @@ import logo from './logo.svg';
 import './App.css';
 import { useDispatch, useSelector } from 'react-redux';
 import { useTypedSelector } from './hooks/useTypedSelector';
-import { fetchNotesActionCreator, postNotesActionCreator } from './store/action-creators/notesActionCreators';
+import { addNoteActionCreator, fetchNotesActionCreator, postNotesActionCreator } from './store/action-creators/notesActionCreators';
+import { INote } from './types/note';
 
 function App() {
 
@@ -18,6 +19,17 @@ function App() {
   useEffect(()=>{
       dispatch(postNotesActionCreator(notes))
   },[notes])
+
+  function testNote(){
+    let note: INote = {
+      id: Date.now(),
+      title: 'Test Note 1',
+      text: 'text text text text text'
+    }
+
+    dispatch(addNoteActionCreator(note))
+  }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -33,6 +45,7 @@ function App() {
         >
           Learn React
         </a>
+        <button onClick={testNote}>add new note</button>
       </header>
     </div>
   );
--- src/store/action-creators/notesActionCreators.ts
+++ src/store/action-creators/notesActionCreators.ts
@@ -3,3 +3,4 @@ import { INote, noteActionTypes } from "../../types/note";
 export const fetchNotesActionCreator = () => ({type: noteActionTypes.FETCH_NOTES});
 export const postNotesActionCreator = (payload: INote[]) => ({type: noteActionTypes.SAVE_NOTES, payload: payload})
 export const setNotesActionCreator = (payload: INote[]) => ({type: noteActionTypes.SET_NOTES, payload: payload})
+export const addNoteActionCreator = (payload: INote) => ({type: noteActionTypes.ADD_NOTE, payload: payload})
