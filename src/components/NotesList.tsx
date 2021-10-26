29b7a20470b4ed4dae75a6f67fa1cffe0698bc75
yarex <yarexmason@gmail.com>
2021-10-26 14:22:18 +0400

    delete function added

--- src/components/NotesList.tsx
+++ src/components/NotesList.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import { useHistory } from "react-router"
 import { INote } from "../types/note"
 import {Stack, Button} from '@chakra-ui/react'
+import { useDispatch } from "react-redux";
+import { deleteNoteActionCreator } from "../store/action-creators/notesActionCreators";
 
 
 interface INotesList {
@@ -11,22 +13,28 @@ interface INotesList {
 
 const NoteList:React.FC<INotesList> = ({notes, chooseNote}) => {
      let history = useHistory();
+     let dispatch = useDispatch();
 
      function showNote(note: INote){
          chooseNote(note)
          history.push('/note')
      }
-     const goToForm = () => history.push('/form')
+     const goToForm = () => history.push('/form');
+     
     return <div>
      <Stack direction="column" spacing={4} align="center" >
         {notes.length? notes.map((note, index)=>{
-            return <Button 
+            return <div><Button 
             width="sm"
             colorScheme="teal" variant="solid"
             onClick={()=>showNote(note)} 
             key={index}>
                 {note.title}
             </Button>
+            <Button onClick={()=>dispatch(deleteNoteActionCreator(note.id))}>
+                delete
+            </Button>
+            </div>
         }): 'there is no notes yet'}
     </Stack>
     <button onClick={goToForm}> add a new note</button>
--- src/store/action-creators/notesActionCreators.ts
+++ src/store/action-creators/notesActionCreators.ts
@@ -5,3 +5,4 @@ export const postNotesActionCreator = (payload: INote[]) => ({type: noteActionTy
 export const setNotesActionCreator = (payload: INote[]) => ({type: noteActionTypes.SET_NOTES, payload: payload})
 export const addNoteActionCreator = (payload: INote) => ({type: noteActionTypes.ADD_NOTE, payload: payload})
 export const updateNoteActionCreator = (payload: UpdateNotePayload) => ({type: noteActionTypes.UPDATE_NOTE, payload: payload})
+export const deleteNoteActionCreator = (payload: number) => ({type: noteActionTypes.DELETE_NOTE, payload:payload})
